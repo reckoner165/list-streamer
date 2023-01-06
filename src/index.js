b4c1391a6f2d5ec881b33ca6a6484385eae59961
Sumanth Srinivasan <sumanth.srinivasan@gmail.com>
2023-01-06 17:20:28 -0500

    Add textbox for domain

--- src/index.js
+++ src/index.js
@@ -2,16 +2,44 @@ import Hls from 'hls.js';
 
 const video = document.getElementById('video');
 const playlistTextBox = document.getElementById('playlist');
+const sourceDomain = document.getElementById('domain');
 
 // bind them together
 
 const submitButton = document.getElementById('submit');
 submitButton.addEventListener('click', () => {
     const playlistText = playlistTextBox.value;
-    // console.log('##', playlistText);
+
+    let expectSegmentUrlInNextLine = false;
+    const lines = playlistText.split('\n');
+    const processedLines = [];
+    
+    for (const line of lines) {
+      let isSpecialCase = false;
+
+      if (expectSegmentUrlInNextLine) {
+          // prepend domain
+          if (!line.startsWith('http')) {
+            let origin = sourceDomain.value;
+            const newLine = origin + line;
+            processedLines.push(newLine);
+            isSpecialCase = true;
+          }
+          expectSegmentUrlInNextLine = false;
+        }
+      
+      if (!isSpecialCase) {
+        processedLines.push(line);
+      }
+
+      if (line.includes('#EXTINF:')) {
+        expectSegmentUrlInNextLine = true;
+      }
+    }
 
     const enc = new TextEncoder();
-    const blob = new Blob([enc.encode(playlistText)]);
+    console.log('###', processedLines);
+    const blob = new Blob([enc.encode(processedLines.join('\n'))]);
 
     const hls = new Hls({
         debug: true,
